f6e6b9b26813ac90e08fcfa916bf5e6d6f10f70b
SamarV-121 <samarvispute121@gmail.com>
2019-03-20 10:01:09 +0000

    Init trees
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/samsung/m20lte
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/samsung/m20lte/prebuilt/Image
+TARGET_PREBUILT_DTB := device/samsung/m20lte/prebuilt/dt.img
+
+# Inherit common board flags
+include device/samsung/universal7885-common/BoardConfigCommon.mk
---
+++ device.mk
@@ -0,0 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Inherit common device configuration
+$(call inherit-product, device/samsung/universal7885-common/universal7885-common.mk)
+
