b64063afdd73b946fbdcbb280480b8d062bf25c4
SamarV-121 <samarvispute121@gmail.com>
2019-03-22 08:49:12 +0000

    m20lte: Enable inline kernel build
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,7 @@
 DEVICE_PATH := device/samsung/m20lte
 
 # Kernel
-TARGET_PREBUILT_KERNEL := device/samsung/m20lte/prebuilt/Image
-TARGET_PREBUILT_DTB := device/samsung/m20lte/prebuilt/dt.img
+TARGET_KERNEL_CONFIG := m20lte_defconfig
 
 # Inherit common board flags
 include device/samsung/universal7885-common/BoardConfigCommon.mk
