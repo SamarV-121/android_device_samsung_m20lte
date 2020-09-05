ccda460c4eb80ce888ce5f70c84a062ab5983ad8
SamarV-121 <samarvispute121@gmail.com>
2020-09-05 06:12:09 +0000

    m20lte: Add system partition size
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,3 +6,6 @@ TARGET_KERNEL_CONFIG := m20lte_defconfig
 
 # Inherit common board flags
 include device/samsung/universal7904-common/BoardConfigCommon.mk
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3565158400
