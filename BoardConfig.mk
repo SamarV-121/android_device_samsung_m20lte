2d5c0518a5e0adcbaba5cd99afc119f9080093d0
SamarV-121 <samarvispute121@gmail.com>
2020-10-16 11:33:47 +0000

    m20lte: Decommonize dtbhtoolExynos and recovery partition size
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,17 @@
 DEVICE_PATH := device/samsung/m20lte
 
 # Kernel
+BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_CONFIG := m20lte_defconfig
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # Inherit common board flags
 include device/samsung/universal7904-common/BoardConfigCommon.mk
 
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3565158400
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
