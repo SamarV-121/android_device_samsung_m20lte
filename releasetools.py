66071ab272dc6c1490b1da4986f075f1fbe0836d
SamarV-121 <samarvispute121@gmail.com>
2020-11-30 14:20:27 +0530

    Revert "[HAX]m20lte: Fulfil dependencies of camera HAL [2/2]"
     * https://github.com/SamarV-121/android_device_samsung_universal7904-common/commit/51739e8e4d1519dbbb5679c8dacda04fff8c6569
    
    This reverts commit e99cdd0ea1e7384e26d0b0c045bef9b3e23ef9dd.

--- releasetools.py
+++ releasetools.py
@@ -14,8 +14,3 @@ def FullOTA_Assertions(info):
     info.script.AppendExtra('ui_print("============NO CHANGE HAS BEEN MADE==========");');
     info.script.AppendExtra('abort(" "););');
     info.script.AppendExtra('unmount("/vendor");');
-
-def FullOTA_InstallEnd(info):
-    info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/13500000.dwmmc0/by-name/SYSTEM", "/system");');
-    info.script.AppendExtra('run_program("/sbin/sh", "/system/system/bin/sys.sh");');
-    info.script.AppendExtra('unmount("/system");');
