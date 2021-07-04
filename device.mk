f183d42b42fed108452a7a11515990de8b8aaab3
SamarV-121 <samarvispute121@gmail.com>
2021-07-04 15:40:49 +0530

    m20lte: Import device specific blobs
    Change-Id: I6094ff63def0c5c145f4d49b46fb78c5446e5b0a
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- device.mk
+++ device.mk
@@ -3,3 +3,4 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal7904-common/universal7904-common.mk)
 
+$(call inherit-product, vendor/samsung/m20lte/m20lte-vendor.mk)
