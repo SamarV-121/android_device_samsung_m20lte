51d49cbcd0a69e129bb05d5ede48f8ed9e2ce45f
SamarV-121 <samarvispute121@gmail.com>
2021-07-04 15:43:41 +0530

    m20lte: Import fstab
     * disabled encryption for now
    
    Change-Id: Ie0f01f82f6fc4d1e9948369674acf3d77f2c6df8
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -4,3 +4,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 $(call inherit-product, device/samsung/universal7904-common/universal7904-common.mk)
 
 $(call inherit-product, vendor/samsung/m20lte/m20lte-vendor.mk)
+
+# fstab
+PRODUCT_PACKAGES += \
+	fstab.exynos7904
