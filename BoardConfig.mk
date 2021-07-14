c242d1de97904054806e97ba6bd2b94103ee6407
SamarV-121 <samarvispute121@gmail.com>
2021-07-14 19:18:33 +0530

    m20lte: Include mobicore sepolicy
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I8674e6f241611eb57c364b1335d02ac8e82e8719

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,12 +6,15 @@ TARGET_KERNEL_CONFIG := m20lte_defconfig
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
-# Inherit common board flags
-include device/samsung/universal7904-common/BoardConfigCommon.mk
-
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3565158400
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/recovery.fstab
+
+# Sepolicy
+BOARD_SEPOLICY_TEE_FLAVOR := mobicore
+
+# Inherit common board flags
+include device/samsung/universal7904-common/BoardConfigCommon.mk
