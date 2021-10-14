1de013d55935bbe1005f7fb0bc82e9721bd769c2
SamarV-121 <samarvispute121@pm.me>
2021-10-14 19:28:30 +0530

    AOSP time
    Change-Id: I5d1211b8e14675baa1e330d61eb0ab2700f27775
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m20lte.mk
+    $(LOCAL_DIR)/lineage_m20lte.mk $(LOCAL_DIR)/m20lte.mk
---
+++ m20lte.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/samsung/m20lte/full_m20lte.mk)
+
+# Inherit some extra fuckery.
+$(call inherit-product, vendor/extra/product.mk)
+
+PRODUCT_NAME := m20lte
