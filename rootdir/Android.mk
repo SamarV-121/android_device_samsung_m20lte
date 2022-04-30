76953115bd25a401c1384cbae267dffd47ed6d5c
SamarV-121 <samarvispute121@pm.me>
2022-04-30 18:53:29 +0530

    m20lte: rootdir: Move to blueprint
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Change-Id: I24cc754bc0d2780c9ca8e7ae8a8ff3d93f8ac546

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,15 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.exynos7904
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-LOCAL_REQUIRED_MODULES := fstab.ramdisk
-include $(BUILD_PREBUILT) 
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.ramdisk
 LOCAL_MODULE_STEM  := fstab.exynos7904
 LOCAL_MODULE_TAGS  := optional
@@ -17,20 +8,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE_STEM)
 LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT) 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.baseband.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT) 
