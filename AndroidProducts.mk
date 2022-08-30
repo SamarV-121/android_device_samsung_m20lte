e9f2572e1b5de66abbc83b883a4a28c64d5e421e
SamarV-121 <samarvispute121@pm.me>
2022-08-30 15:00:14 +0000

    m20lte: Leafify
    Change-Id: Ied34b018ad27bdbb45cb1247ed5136de80090f4b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m20lte.mk
+    $(LOCAL_DIR)/lineage_m20lte.mk $(LOCAL_DIR)/leaf_m20lte.mk
---
+++ leaf_m20lte.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/samsung/m20lte/full_m20lte.mk)
+
+# Inherit some common Leaf stuff.
+$(call inherit-product, vendor/leaf/config/common_full_phone.mk)
+
+PRODUCT_NAME := leaf_m20lte
