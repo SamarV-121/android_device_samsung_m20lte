ced9cfe4d8e9b76d13c7943fd0e9f4191de5d519
SamarV-121 <samarvispute121@pm.me>
2022-11-10 09:59:58 +0530

    m20lte: Adapt for leaf
    Change-Id: I400cdc4f9b342c42d340cdeea3d9e1cfea173fe5

--- lineage_m20lte.mk
+++
@@ -1,6 +0,0 @@
-$(call inherit-product, device/samsung/m20lte/full_m20lte.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_m20lte
---
+++ m20lte.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/samsung/m20lte/full_m20lte.mk)
+
+# Inherit some common LeafOS stuff.
+$(call inherit-product, vendor/leaf/config/common_full_phone.mk)
+
+PRODUCT_NAME := m20lte
