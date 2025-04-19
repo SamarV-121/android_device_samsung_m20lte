cf482f5d65271939d4232638448e9e495ee293ac
royna2544 <whiteshell2544@naver.com>
2025-04-19 19:05:53 +0900

    m20lte: Migrate to soong_config_set
    Change-Id: Id698c927cadac6382d42b56a160fe6d705f7da58

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 DEVICE_PATH := device/samsung/m20lte
 
 # Camera
-BOARD_HWJPEG_ANDROID_VERSION := 10
+$(call soong_config_set,libhwjpeg,BOARD_HWJPEG_ANDROID_VERSION,10)
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_m20lte
